87440f791cc4ad957592d055619bdca58c10aa2d
Alexander Bondarenko <aenor.realm@gmail.com>
2017-08-13 21:20:57 +0300

    Reformat code in dictionary example

--- soap/examples/dictionary.hs
+++ soap/examples/dictionary.hs
@@ -1,7 +1,9 @@
 #!/usr/bin/env stack
-{- stack --install-ghc --resolver lts-8.23 runghc --package soap -}
+{- stack runghc --package soap -}
 {-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -Wall -Werror -fno-warn-unused-imports #-}
 
 import           Control.Monad               (forM_)
 import           Data.Text                   (Text)
@@ -16,62 +18,62 @@ import           Text.XML.Cursor             (Cursor, content, laxElement, ($/),
                                               (&/), (&//))
 import qualified Text.XML.Writer             as W
 
-data Response = Response {
-    rWord :: Text,
-    rDefs :: [Definition]
-} deriving (Show)
+data DefineResult = DefineResult
+    { drWord :: Text
+    , drDefs :: [Definition]
+    } deriving (Show)
 
-data Definition = Definition {
-    dWord :: Text,
-    dDict :: Dictionary,
-    dDef  :: Text
-} deriving (Show)
+data Definition = Definition
+    { dWord :: Text
+    , dDict :: Dictionary
+    , dDef  :: Text
+    } deriving (Show)
 
-data Dictionary = Dictionary {
-    diId   :: Text,
-    diName :: Text
-} deriving (Show)
-
-body :: Text -> W.XML
-body word = W.elementA "Define" [("xmlns", "http://services.aonaware.com/webservices/")]
-        $ W.element "word" (word :: Text)
-
-textOf :: Cursor -> Text -> Text
-textOf c t = T.concat $ c $/ laxElement t &// content
-
-def :: Cursor -> [Definition]
-def c = let
-        dict :: Cursor = head $ c $/ laxElement "Dictionary"
-    in
-        [Definition (textOf c "Word")
-                    (Dictionary (textOf dict "id") (textOf dict "Name"))
-                    (textOf c "WordDefinition")]
-
-parser :: Cursor -> Response
-parser c = let
-        res = head (c $/ laxElement "DefineResponse" &/ laxElement "DefineResult")
-        defs = res $/ laxElement "Definitions" &/ def
-    in
-        Response (textOf res "Word") defs
+data Dictionary = Dictionary
+    { diId   :: Text
+    , diName :: Text
+    } deriving (Show)
 
 main :: IO ()
 main = do
     args <- map T.pack <$> getArgs
 
     -- find WSDL here: http://services.aonaware.com/dictservice/dictservice.asmx?wsdl
-
-    transport <- initTransportWithM defaultManagerSettings
-                 "http://services.aonaware.com/DictService/DictService.asmx"
-                 return return
-                 -- instead of "return return" you can use "printRequest printBody"
+    transport <- initTransportWithM
+        defaultManagerSettings
+        "http://services.aonaware.com/DictService/DictService.asmx"
+        pure -- or printRequest
+        pure -- or printBody
 
     forM_ args $ \word -> do
-        r <- invokeWS transport "http://services.aonaware.com/webservices/Define"
-                () (body word) (CursorParser parser)
+        DefineResult{drWord, drDefs} <- define transport word
 
-        putStrLn $ "Word definitions of " ++ show (rWord r)
-        forM_ (rDefs r) $ \d -> do
-            T.putStrLn $ T.concat ["=== SOURCE: ", diName $ dDict d, " ==="]
-            T.putStrLn $ dDef d
+        putStrLn $ "Word definitions of " ++ show drWord
+        forM_ drDefs $ \Definition{dDict, dDef} -> do
+            T.putStrLn $ T.concat ["=== SOURCE: ", diName dDict, " ==="]
+            T.putStrLn dDef
 
+define :: Transport -> Text -> IO DefineResult
+define transport word = invokeWS transport url () body (CursorParser parser)
+   where
+    url = "http://services.aonaware.com/webservices/Define"
 
+    body =
+        W.elementA "Define" [("xmlns", "http://services.aonaware.com/webservices/")] $
+            W.element "word" word
+
+    parser cur = DefineResult (textOf res "Word") defs
+      where
+        res = head $ cur $/ laxElement "DefineResponse" &/ laxElement "DefineResult"
+        defs = res $/ laxElement "Definitions" &/ def
+        def c =
+            [ Definition
+                (textOf c "Word")
+                (Dictionary (textOf dict "id") (textOf dict "Name"))
+                (textOf c "WordDefinition")
+            ]
+          where
+            dict = head $ c $/ laxElement "Dictionary"
+
+textOf :: Cursor -> Text -> Text
+textOf c t = T.concat $ c $/ laxElement t &// content
